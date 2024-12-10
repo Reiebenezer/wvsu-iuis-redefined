f5a660c31e1039c8b3d2cc15821b7de2828d68d8
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2024-12-10 13:41:00 +0800

    reverted firebase config (previous commit)

--- src/lib/ts/firebase.app.ts
+++ src/lib/ts/firebase.app.ts
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 
-const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } = process.env; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
-  measurementId,
+    apiKey: 'AIzaSyAQss35zR0QBonSiQRDrn13ky_Fv8rf6kQ',
+    authDomain: 'iuis-v2.firebaseapp.com',
+    projectId: 'iuis-v2',
+    storageBucket: 'iuis-v2.firebasestorage.app',
+    messagingSenderId: '586224800415',
+    appId: '1:586224800415:web:251915971b138cb26488aa',
+    measurementId: 'G-C12EEVFGXD',
 };
 
 // Initialize Firebase
